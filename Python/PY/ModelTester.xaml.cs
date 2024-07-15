57cd0e63b79b7d18376e7c697edd1e8cdcc7c4ef
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2024-07-15 13:27:16 +0700

    Patch

--- Python/PY/ModelTester.xaml.cs
+++ Python/PY/ModelTester.xaml.cs
@@ -29,7 +29,7 @@ namespace Lithicsoft_Trainer_Studio.Python.PY
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            if (File.Exists($"projects\\{projectName}\\outputs\\model.zip"))
+            if (File.Exists($"projects\\{projectName}\\outputs"))
             {
                 button1.IsEnabled = true;
             }
