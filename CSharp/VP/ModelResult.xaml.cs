9857692646daf3486ac4f014a13397ae3abce5d7
Bùi Nguyễn Tấn Sang <endermatday@gmail.com>
2024-07-15 11:49:30 +0700

    Patch

--- CSharp/VP/ModelResult.xaml.cs
+++ CSharp/VP/ModelResult.xaml.cs
@@ -29,7 +29,7 @@ namespace Lithicsoft_Trainer_Studio.CSharp.VP
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            if (File.Exists($"project\\{projectName}\\outputs\\model.zip"))
+            if (File.Exists($"projects\\{projectName}\\outputs\\model.zip"))
             {
                 button1.IsEnabled = true;
             }
